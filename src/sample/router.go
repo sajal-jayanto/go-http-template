a77af8bf4545cb1d6b9769df765a41eff573478b
sajal-jayanto <sajaljayanto007@gmail.com>
2025-08-10 02:20:11 +0600

    formate

--- src/sample/router.go
+++ src/sample/router.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func (handler *Handler) RegisterRoutes(router *gin.RouterGroup) {
 
 	router.POST("/",   handler.CreateSample)
-	router.GET("/", handler.FindAllSample)
+	router.GET("/",    handler.FindAllSample)
 	router.GET("/:id", handler.FindOneSample)
 
 }
